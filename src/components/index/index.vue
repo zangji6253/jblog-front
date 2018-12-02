128a3cad874cdc3fded9cdf6e1ec76f79f72727f
zhangjian <184263567@qq.com>
2018-12-03 05:17:30 +0800

    使用路由搭建index

---
+++ src/components/index/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="index">
+      <h1>aaa</h1>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "index"
+    }
+</script>
+
+<style scoped>
+
+</style>
