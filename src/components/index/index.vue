4502108437e6f973179010553f20c29074cb2ac4
Jian Zhang <184263567@qq.com>
2018-12-03 09:08:13 +0800

    首页左右布局

--- src/components/index/index.vue
+++ src/components/index/index.vue
@@ -1,6 +1,27 @@
 <template>
   <div class="index">
-      <h1>aaa</h1>
+    <div class="container-fluid">
+      <div id="flex">
+        <div id="left">
+          <h1>标题2</h1>
+          <p>
+            内容
+          </p>
+          <a href="/edit/1">编辑</a>
+        </div>
+        <div id="right">
+          <a href="/insert">新增</a>
+          <ul>
+            <li>
+              <a href="">标题1</a>
+            </li>
+            <li>
+              <a href="">标题2</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,4 +33,18 @@
 
 <style scoped>
 
+</style>
+
+<style type="text/css">
+  #flex {
+    display: flex;
+  }
+
+  #left {
+    flex: 1 1 auto;
+  }
+
+  #right {
+    width: 200px;
+  }
 </style>
